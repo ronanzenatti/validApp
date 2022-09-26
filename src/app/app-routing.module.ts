2f38318f4d37c1aff9ef20765a742a64b1c8dd0d
Ronan Adriel Zenatti <ronanzenatti@gmail.com>
2022-09-26 20:18:28 -0300

    calc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'calc',
+    loadChildren: () => import('./calc/calc.module').then( m => m.CalcPageModule)
+  },
 ];
 
 @NgModule({
