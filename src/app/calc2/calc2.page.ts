29aff3d5b12bc11ae9fa599dd0afee0b8d1d944f
Ronan Adriel Zenatti <ronanzenatti@gmail.com>
2022-10-03 20:40:29 -0300

    update calc2

--- src/app/calc2/calc2.page.ts
+++ src/app/calc2/calc2.page.ts
@@ -6,10 +6,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calc2.page.scss'],
 })
 export class Calc2Page implements OnInit {
+  operacao = '';
+  resultado = '';
+  numero = false;
+  caracter = true;
+  caracteres = [',', '/', '*', '+', '-'];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  adicionarValor(valor: string) {
+    this.caracter = this.caracteres.includes(valor);
+
+    if (!this.caracter) {
+      this.operacao += valor;
+      this.numero = true;
+    } else if (this.caracter && this.numero) {
+      this.operacao += valor;
+      this.numero = false;
+    }
   }
 
+  limparMemoria() {
+    this.operacao = '';
+    this.resultado = '';
+    this.numero = false;
+  }
+
+  limparOperacao() {
+    this.operacao = '';
+    this.numero = false;
+  }
+
+  inverterValor() {
+    // Alterna o numero entre positivo e negativo
+  }
+
+  apagarCaracter() {
+    if (this.operacao.length > 0) {
+      this.operacao = this.operacao.substring(0, this.operacao.length - 1);
+    }
+
+    const ultimo = this.operacao.substring(-1);
+    this.caracter = this.caracteres.includes(ultimo);
+
+    if (!this.caracter) {
+      this.numero = true;
+    } else {
+      this.numero = false;
+    }
+  }
 }
